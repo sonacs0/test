6f91673fdb601de19fcd6b55ca336150b1bff01f
sonacs0 <cssona30@gmail.com>
2022-02-07 20:25:37 +0530

    test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './sharepage/navbar/navbar.component';
+import { PageComponent } from './sharepage/page/page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    PageComponent
   ],
   imports: [
     BrowserModule,
